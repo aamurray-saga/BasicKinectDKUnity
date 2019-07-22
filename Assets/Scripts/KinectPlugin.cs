2803510d223688e3561893e05afc3a10cad5e45c
aamurray-saga <alexandre@play-lu.com>
2019-07-22 09:27:45 -0400

    Basic Unity project that tries to Open and Start the default Kinect camera via a native plugin

---
+++ Assets/Scripts/KinectPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class KinectPlugin : MonoBehaviour
+{
+    [DllImport("KinectPlugin")]
+    private static extern bool KinectPlugin_OpenDefaultDevice();
+
+    void Awake()
+    {
+        Debug.Log("OpenDefaultDevice result = " + KinectPlugin_OpenDefaultDevice());
+    }
+}
